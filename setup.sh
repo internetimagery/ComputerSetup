b7f1460166ced2657ad4d46c6fb8f157425c41c9
Internetimagery <jason.dixon.email@gmail.com>
2015-08-20 03:11:01 +1200

    added nodejs install

--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ sudo apt-get install skype
 
 # Install development
 sudo apt-get install gcc g++ alien git python ntfs-3g pylint coffeescript
+sh ./Nodejs/node-install.sh
 
 # Install custom mouse setup packages
 sudo apt-get install -y xbindkeys
@@ -33,6 +34,7 @@ export DownloadDIR=$HOME/temp_download
 if [ ! -d $DownloadDIR ]; then
   mkdir $DownloadDIR
 fi
+cd $DownloadDIR
 
 # Install ATOM editor
 export ATOM=$DownloadDIR/atom.deb
@@ -41,3 +43,14 @@ if [ ! -f $ATOM ]; then
   wget https://atom.io/download/deb -O $ATOM
 fi
 sudo dpkg -i $ATOM
+
+# Install NodeJs
+if [ ! -f node.html ]; then
+  wget https://nodejs.org/dist/latest/ -O node.html
+fi
+export NodeVersion=$(cat node.html | grep -o -E 'href="(node-v.*-linux-x64.tar.gz)"' | cut -d'"' -f2 | sort | uniq)
+if [ ! -f $NodeVersion ]; then
+  wget https://nodejs.org/dist/latest/$NodeVersion -O $NodeVersion
+fi
+cd /usr/local && sudo tar --strip-components 1 -xzvf $DownloadDIR/$NodeVersion
+cd $DownloadDIR
